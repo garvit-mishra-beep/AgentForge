f3b0d5787101c2d68656bf8610bd11521f573326
Student Name <garvit2006@gmail.com>
2026-06-08 15:24:18 +0530

    Initial commit - Add all Java project folders

---
+++ OneDrive/Desktop/JAVA/leetcode/L2.java
@@ -0,0 +1,16 @@
+class L2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 != null ? l1.val : 0) + 
+                      (l2 != null ? l2.val : 0) + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        return dummy.next;
+    }
+}
